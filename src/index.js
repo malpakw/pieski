18133996b87bc2dd77eb289929cc2c44a11fea3f
malpakw <marcin.zietara.kw@gmail.com>
2022-05-22 14:51:20 +0200

    scss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
-import './styles/main.scss';
+import './sass/style.scss';
 
-function listBreeds () {
-    fetch('https://dog.ceo/api/breeds/list/all')
-    .then (resp => resp.json())
-    .then(data => {
-        console.log(data);
-    })
+class Doggo {
+
+    constructor() {
+        this.apiUrl = 'https://dog.ceo/api';
+        this.imgEl = document.querySelector('.featured-dog img');
+        this.backgroundEl = document.querySelector('.featured-dog__background');
+
+        this.init();
+
+    }
+
+    listBreeds() {
+        return fetch(`${this.apiUrl}/breeds/list/all`)
+            .then(resp => resp.json())
+            .then(data => data.message);
+    }
+    
+    getRandomImage() {
+        return fetch(`${this.apiUrl}/breeds/image/random`)
+            .then (resp => resp.json())
+            .then(data => data.message)  
+    }
+    
+    getRandomImageByBreed(breed) {
+        return fetch(`${this.apiUrl}/breed/${breed}/images/random`)
+            .then (resp => resp.json())
+            .then (data => data.message)
+    }
+
+    init() {
+        this.getRandomImage()
+            .then(src => {
+                this.imgEl.setAttribute('src', src);
+                this.backgroundEl.style.background = `url("${src}")`
+    });
+
+        this.listBreeds()
+            .then(breeds => console.log(breeds));
+    }
 }
 
-listBreeds();
+document.addEventListener('DOMContentLoaded', () => {
+    new Doggo();
+})
+
+
+// const imgTag = document.querySelector('img');
+
+// getRandomImageByBreed('bulldog/french')
+// .then(imgSrc => imgTag.setAttribute('src', imgSrc))
